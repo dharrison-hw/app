60e869b343f94e7244653913fdff6b890b6adf8b
Dan Harrison <hwyfour@gmail.com>
2016-11-02 09:59:10 -0700

    added more functionality

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
+import json
+import uuid
+
+import hyperwallet
+
 from flask import Flask, render_template, request, redirect, url_for
+
+api = hyperwallet.Api(
+    "restapiuser@5046781612",
+    "Password1$",
+    "prg-15cefcdc-2363-4bf0-ab3e-b7d9959bcdd7",
+    "https://api.sandbox.hyperwallet.com"
+)
+
+user = {}
 
 app = Flask(__name__)
 
@@ -6,16 +20,51 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
 @app.route('/create')
 def create():
-    return render_template('create.html')
+    return render_template('profile.html')
+
+
+@app.route('/add_bank')
+def add_bank():
+    return render_template('add_bank.html', name = user["firstName"])
+
+
+@app.route('/transfer')
+def transfer():
+    return render_template('transfer_method.html', token = user["token"])
+
+
+@app.route('/create_transfer_method', methods=['POST'])
+def create_transfer_method():
+
+    headers = {"Json-Cache-Token": request.form['hw_json_cache_trm_token']}
+
+    response = api.createTransferMethod(user["token"], {}, headers)
+
+    return redirect(url_for('home'))
+
 
 @app.route('/create_user', methods=['POST'])
 def create_user():
 
-    name = request.form['user_name']
-    email = request.form['user_mail']
+    user["profileType"] = "INDIVIDUAL"
+    user["clientUserId"] = str(uuid.uuid4())
+    user["firstName"] = request.form["firstName"]
+    user["lastName"] = request.form["lastName"]
+    user["email"] = request.form["email"]
+    user["dateOfBirth"] = request.form["dateOfBirth"]
+    user["addressLine1"] = request.form["addressLine1"]
+    user["city"] = request.form["city"]
+    user["country"] = request.form["country"]
+    user["stateProvince"] = request.form["stateProvince"]
+    user["postalCode"] = request.form["postalCode"]
 
-    print name, email
+    response = api.createUser(user)
 
-    return redirect(url_for('home'))
+    user["token"] = response.json()["token"]
+
+    return redirect(url_for('add_bank'))
+
+app.run(debug=True)
